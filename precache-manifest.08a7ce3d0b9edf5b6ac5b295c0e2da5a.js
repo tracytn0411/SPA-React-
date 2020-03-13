c08771d31a66bc14abddab368f80438fd4926b21
Tracy Nguyen <tracytn0411@gmail.com>
2020-03-13 02:03:56 -0700

    Updates

---
+++ precache-manifest.08a7ce3d0b9edf5b6ac5b295c0e2da5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9481b8bfe061d7602a31ad85aed759a2",
+    "url": "/tracytn0411/spa-react-router/index.html"
+  },
+  {
+    "revision": "18672a3c101356632e90",
+    "url": "/tracytn0411/spa-react-router/static/css/main.eeb2cc49.chunk.css"
+  },
+  {
+    "revision": "ee5d2d8d53114bad22e4",
+    "url": "/tracytn0411/spa-react-router/static/js/2.ed1cf797.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/tracytn0411/spa-react-router/static/js/2.ed1cf797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18672a3c101356632e90",
+    "url": "/tracytn0411/spa-react-router/static/js/main.c957e561.chunk.js"
+  },
+  {
+    "revision": "a3171a3ba32ef3bce4f8",
+    "url": "/tracytn0411/spa-react-router/static/js/runtime-main.c3fc0181.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tracytn0411/spa-react-router/static/media/logo.5d5d9eef.svg"
+  }
+]);
